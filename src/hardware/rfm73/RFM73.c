ca6a2641f15d17fd89d1152efa4cbdd291880c08
naomai <lulu@nieznani-sprawcy.pl>
2025-12-29 23:15:07 +0100

    fix: improve RFM73 performance by hackish init

--- src/hardware/rfm73/RFM73.c
+++ src/hardware/rfm73/RFM73.c
@@ -7,9 +7,15 @@
 
 #include <stdbool.h>
 #include <stdint.h>
+#include <stddef.h>
 #include "RFM73.h"
 #include "defs.h"
+#include <avr/pgmspace.h>
 #include "platform_shim.h"
+
+#define count(x) sizeof(x)/sizeof(x[0])
+
+
 
 
 #define PACKET_LEN 32
@@ -38,6 +44,7 @@ uint8_t RFM73_RegisterAnd(uint8_t addr, uint8_t mask) __attribute__((noinline)) 
 uint8_t RFM73_RegisterOr(uint8_t addr, uint8_t mask) __attribute__((noinline)) BOOTLOADER_SECTION;
 void RFM73_WriteAddressRegs(uint32_t addr) __attribute__((noinline)) BOOTLOADER_SECTION;
 uint8_t RFM73_GetStatus();
+void RFM73_UglyInit();
 
 struct {
 	uint16_t totalTX;
@@ -47,6 +54,49 @@ struct {
 } radioStats;
 
 uint32_t myAddress = 0;
+
+const uint8_t PROGMEM REGS1[][4] ={
+	//MIXED BYTE ORDER!! Check docs in SPI Command section
+	/* MS-byte to LS-byte in Bank1 registers 0-8 & 15 */
+	/* LS-byte to MS-byte in Bank0 & Bank1 registers 9-14 */
+	{0x40, 0x4B, 0x01, 0xE2}, // Must write with 0x404B01E2
+	{0xC0, 0x4B, 0x00, 0x00}, // Must write with 0xC04B0000
+	{0xD0, 0xFC, 0x8C, 0x02}, // Must write with 0xD0FC8C02
+		
+		
+	// more info: AN0007(BK2423)
+	// REG3
+	//{0x99, 0x00, 0x39, 0x41}, // Must write with 0x99003941
+	{0xF9, 0x00, 0x39, 0x41}, // BK2423 - high transmission power mode
+		//bit31~29: 111; Rvco_tx[2:0]=Bank1_REG3[31:29]
+	
+	// REG4
+	//{0xD9, 0xBE, 0x86, 0x0B}, // BK2423 - Must write with 0xD99E860B (sic!)
+		 //bit21: High sensitivity mode (different CD detection values))
+	{0xF9, 0xBE, 0x84, 0x0B}, // BK2423 - high transmission power mode 
+		 //bit20:      1; RF_PWR[2]=Bank1_REG4[20]
+		 //bit09:      0; IctrlPA[0]=Bank1_REG4[9]
+		 //bit29~27: 111; txIctrl[2:0]=Bank1_REG4[29:27]
+	//{0xF9, 0x96, 0x82, 0x1B}, //  BK2425 - 1Msps: 0xF996821B
+		
+	// REG5
+	{0x24, 0x02, 0x0F, 0xA6}, // Must write with 0x24060FA6 (sic! bit18: RSSI measurement Enable)
+	{0x00, 0x00, 0x00, 0x00},
+	{0x00, 0x00, 0x00, 0x00},
+	{0x00, 0x00, 0x00, 0x00},
+	{0x00, 0x00, 0x00, 0x00},
+	{0x00, 0x00, 0x00, 0x00},
+	{0x00, 0x00, 0x00, 0x00},
+	{0x00, 0x12, 0x73, 0x05}, // Please initialize with 0x05731200
+	{0x36, 0xb4, 0x80, 0x00}  // Please initialize with 0x0080B436
+};
+
+const uint8_t PROGMEM REG1_E[] ={
+	// BK2423: Ramp curve, MS-to-LS 0xFFEF7DF208082082041041
+	0xFF, 0xEF, 0x7D, 0xF2, 0x08, 0x08, 0x20, 0x82, 0x04, 0x10, 0x41
+	// BK2425: Ramp curve, MS-to-LS 0xFFFFFEF7CF208104082041
+	//0xFF, 0xFF, 0xFE, 0xF7, 0xCF, 0x20, 0x81, 0x04, 0x08, 0x20, 0x41
+};
 
 void RFM73_Init(){
 	RFM73_InitInterface();
@@ -60,6 +110,8 @@ void RFM73_InitChip(){
 
 	RFM73_PowerDown();
 	
+	RFM73_UglyInit();
+	
 	// setup address width to 4 bytes
 	RFM73_WriteRegisterByte(SETUP_AW, 0x02);
 	//250kbps, +5 dBm, LNA high gain
@@ -72,7 +124,7 @@ void RFM73_InitChip(){
 	RFM73_WriteRegisterByte(EN_AA, (1<<ENAA_P0));
 	//retransmission
 	
-	RFM73_WriteRegisterByte(SETUP_RETR, 0x34); // 750ms, 4 retransmissions
+	RFM73_WriteRegisterByte(SETUP_RETR, 0x26); // 500us, 6 retransmissions
 	//from datasheet of nRF24L01+:
 	//"In 250kbps mode (even when the payload is not in ACK) the ARD must be 500uS or more."
 	
@@ -176,7 +228,7 @@ bool RFM73_Transmit(uint32_t addr, uint8_t *data, uint8_t length){
 		RFM73_SwapByte(data[i]);
 	}
 	RFM73_SelectChip(false);
-	
+	delayUs(5);
 	RFM73_EnableChip(true);
 	delayUs(15);
 	RFM73_EnableChip(false);
@@ -238,6 +290,7 @@ void RFM73_EnableNewFeatures(){
 }
 
 bool RFM73_IsRadioPresent(){
+	// check radio presence by switching register banks
 	uint8_t status, bankStart;
 	bool radioPresent = false;
 
@@ -252,7 +305,9 @@ bool RFM73_IsRadioPresent(){
 	
 	if(radioPresent){
 		RFM73_SwitchBank(0);
-
+		uint8_t pipe5addr;
+		RFM73_ReadRegister(0xF, &pipe5addr, 1);
+		radioPresent = (pipe5addr==0xC6);
 	}
 	
 	return radioPresent;
@@ -358,4 +413,29 @@ bool RFM73_IsDataAvailable(){
 		}
 	}
 	return false;
+}
+
+void RFM73_UglyInit(){
+	// initialize registers with "mystery magic values"
+	uint8_t regVal[4], regValE[sizeof(REG1_E)];
+		
+	/*// init PLL_LOCK (from 'RFM73 Replace RFM70 Precautions')
+	RFM73_WriteRegisterByte(RF_SETUP, 0x0F);*/
+	
+	RFM73_SwitchBank(1);
+	for(uint8_t i=0; i<count(REGS1); i++){
+		memcpy_P(&regVal, &REGS1[i], sizeof(REGS1[0]));
+		RFM73_WriteRegister(i, &regVal, 4);
+	}
+	
+	/*//http://www.voti.nl/rfm73/index.html
+	memcpy_P(&regVal, &REG1_4, sizeof(REG1_4));
+	RFM73_WriteRegister(0x04, &regVal, 4);*/
+	
+	memcpy_P(&regValE, &REG1_E, sizeof(REG1_E));
+	RFM73_WriteRegister(0x0E, &regValE, sizeof(REG1_E));
+	
+	
+	
+	RFM73_SwitchBank(0);
 }
